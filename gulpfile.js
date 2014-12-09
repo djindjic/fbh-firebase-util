696958c5c4e247708e8d527e9086aa20ca7f767a
Aleksandar Djindjic <djindjic@gmail.com>
2014-12-09 23:57:43 +0100

    fix package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var gulp_runner = require('gulp'),
-    shell_link  = require('gulp-shell'),
-    watch_files = require('gulp-watch');
-
-gulp_runner.task('default', function(cb) {
-  watch_files(['lib/**/*'], shell_link.task(['jspm link github:djindjic/fbh-firebase-util@0.0.14 -y']));
+var gulp  = require('gulp'),
+    shell = require('gulp-shell'),
+    watch = require('gulp-watch');
+ 
+gulp.task('default', function(cb) {
+  watch(['lib/**/*'], shell.task(['jspm link github:djindjic/fbh-firebase-util@0.0.14 -y']));
 });
