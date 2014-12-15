dc4c8d0aed460a15d9e90a057a3e4f19f334b350
Aleksandar Djindjic <djindjic@gmail.com>
2014-12-15 20:15:44 +0100

    store user

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,5 @@ var gulp  = require('gulp'),
     watch = require('gulp-watch');
  
 gulp.task('default', function(cb) {
-  watch(['lib/**/*'], shell.task(['jspm link github:djindjic/fbh-firebase-util@0.0.19 -y']));
+  watch(['lib/**/*'], shell.task(['jspm link github:djindjic/fbh-firebase-util@0.0.20 -y']));
 });
