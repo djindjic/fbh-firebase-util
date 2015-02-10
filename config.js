6ffdc40b99637cf2ac7ce6e9933a020fad4de51d
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-10 02:32:43 +0100

    current user

--- config.js
+++ config.js
@@ -2,17 +2,17 @@ System.config({
   "paths": {
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js",
-    "app/*": "lib/*.js"
+    "fbh-firebase-util/*": "lib/*.js"
   }
 });
 
 System.config({
   "map": {
-    "angular": "github:angular/bower-angular@1.3.8",
-    "angularfire": "github:firebase/angularfire@0.9.0",
-    "github:firebase/angularfire@0.9.0": {
-      "angular": "github:angular/bower-angular@1.3.8",
-      "firebase": "github:firebase/firebase-bower@2.0.6"
+    "angular": "github:angular/bower-angular@1.3.13",
+    "angularfire": "github:firebase/angularfire@0.9.2",
+    "github:firebase/angularfire@0.9.2": {
+      "angular": "github:angular/bower-angular@1.3.13",
+      "firebase": "github:firebase/firebase-bower@2.1.2"
     }
   }
 });
